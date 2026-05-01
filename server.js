e587c0089f992296b5d3ed3274ab9382e7449219
Anjalishah1234 <iamanjalishah123@gmail>
2026-05-01 16:53:50 +0530

    fix railway port issue

--- server.js
+++ server.js
@@ -131,6 +131,8 @@ app.post("/tasks", async (req, res) => {
     }
 });
 
-app.listen(8000, () => {
-    console.log("Server running on port 8000");
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
